a3daeb9c5cdfd61a6000ac252352df6bab163736
Ben Brunton <ben.brunton@infinityworks.com>
2018-02-18 20:24:21 +0000

    include icons

--- src/decorate/mod.rs
+++ src/decorate/mod.rs
@@ -5,6 +5,7 @@ use fs::File;
 use style::PaintItem;
 use ansi_term::Colour;
 use std::collections::BTreeMap;
+use std::str::FromStr;
 
 pub struct Decorate<'a>{
     config: Option<&'a Store>
@@ -33,6 +34,12 @@ impl <'a> Decorate<'a> {
             _    => self.get_colour(&file.get_name())
         };
 
+
+        let icon = match file.get_is_dir() {
+            true => self.get_dir_icon(&file.get_name()),
+            _    => self.get_icon(&file.get_name())
+        };
+
         PaintItem{
             label,
             is_bold: false,
@@ -40,71 +47,80 @@ impl <'a> Decorate<'a> {
             is_dimmed: false,
             is_hidden,
             colour: colour,
-            icon: None
+            icon: icon
         }
     }
 
     fn get_colour(&self, path: &str) -> Option<Colour> {
         match self.get("colour") {
+            Some(&toml::Value::Table(ref colour_table)) => self.search_colour_table(path, colour_table),
+            _ => None
+        }
+    }
 
-            Some(&toml::Value::Table(ref colour_table)) => {
-
-                for (key, colour) in colour_table {
-                    let applied_colour = self.get_colour_from_key(key);
-
-                    if self.matches_glob_array(colour.get("except"), path) {
-                        continue;
-                    }
-
-                    if self.matches_glob_array(colour.get("colour"), path) {
-                        return Some(applied_colour);
-                    }
-                }
-
-                None
-            },
+    fn get_icon(&self, path: &str) -> Option<char> {
+        match self.get("icon") {
+            Some(&toml::Value::Table(ref icon_table)) => self.search_icon_table(path, icon_table),
             _ => None
         }
     }
 
     fn get_dir_colour(&self, path: &str) -> Option<Colour> {
-
-        match self.get("dir") {
-            
-            Some(&toml::Value::Table(ref dir_table)) => {
-                match dir_table.get("colour") {
-                    Some(&toml::Value::Table(ref colour_table)) => self.search_colour_table(path, colour_table),
-                    _ => None
-
-                }
-
-            },
+        match self.get_from_dir("colour") {
+            Some(&toml::Value::Table(ref colour_table)) => self.search_colour_table(path, colour_table),
             _ => None
 
         }
     }
 
+    fn get_dir_icon(&self, path: &str) -> Option<char> {
+        match self.get_from_dir("icon") {
+            Some(&toml::Value::Table(ref icon_table)) => self.search_icon_table(path, icon_table),
+            _ => None
+        }
+    }
+
     fn search_colour_table(&self, path: &str, colour_table: &BTreeMap<String, toml::Value>) -> Option<Colour> {
+        for (key, colour) in colour_table {
+            let applied_colour = self.get_colour_from_key(key);
 
-            for (key, colour) in colour_table {
-                let applied_colour = self.get_colour_from_key(key);
-
-                if self.matches_glob_array(colour.get("except"), path) {
-                    continue;
-                }
-
-                if self.matches_glob_array(colour.get("colour"), path) {
-                    return Some(applied_colour);
-                }
+            if self.matches_glob_array(colour.get("except"), path) {
+                continue;
             }
 
-            None
+            if self.matches_glob_array(colour.get("colour"), path) {
+                return Some(applied_colour);
+            }
+        }
+        None
+    }
 
+    fn search_icon_table(
+        &self,
+        path: &str,
+        icon_table: &BTreeMap<String, toml::Value>
+    ) -> Option<char> {
+        for (_, icon_info) in icon_table {
+            if self.matches_glob_array(icon_info.get("except"), path) {
+                continue;
+            }
+
+            if self.matches_glob_array(icon_info.get("files"), path) {
+                match icon_info.get("icon") {
+                    Some(&toml::Value::String(ref icon)) => {
+                        let ch = icon.chars().next().unwrap_or('\u{1F5CB}');
+                        return Some(
+                            ch
+                        );
+                    },
+                    _ => return None
+                }
+            }
+        }
+        None
     }
 
     fn get_colour_from_key(&self, key: &str) -> Colour {
-
-
         match key {
             "black"     => Colour::Black,
             "red"       => Colour::Red,
@@ -119,9 +135,7 @@ impl <'a> Decorate<'a> {
     }
 
     fn is_hidden(&self, path: &str) -> bool {
-
         match self.get("hidden") {
-
             Some(&toml::Value::Table(ref hidden_table)) => {
 
                 if self.matches_glob_array(hidden_table.get("except"), path) {
@@ -136,7 +150,6 @@ impl <'a> Decorate<'a> {
                 false
             },
             _ => false
-
         }
     }
 
@@ -148,9 +161,15 @@ impl <'a> Decorate<'a> {
 
     }
 
+    fn get_from_dir(&self, key: &'static str) -> Option<&toml::Value> {
+        match self.get("dir") {
+            Some(&toml::Value::Table(ref dir_table)) => dir_table.get(key),
+            _ => None
+        }
+    }
+
 
     fn matches_glob_array(&self, glob_array: Option<&toml::Value>, path: &str) -> bool {
-
         match glob_array {
             Some(&toml::Value::Array(ref globs)) => {
                 for glob in globs {
